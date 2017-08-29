a7678f47ea9646e9b4467fc37bdee1d52b15c4e9
Ruben Unteregger <ruben.unteregger@gmail.com>
2017-08-29 14:23:56 +0200

    Fix TargetList handling

--- MinaryLib/AttackService/ServiceParameters.cs
+++ MinaryLib/AttackService/ServiceParameters.cs
@@ -1,5 +1,8 @@
 ﻿namespace MinaryLib.AttackService
 {
+  using System.Collections.Generic;
+
+
   public class ServiceParameters
   {
 
@@ -7,6 +10,7 @@
 
     private int selectedIfcIndex;
     private string selectedIfcId;
+    private Dictionary<string, string> targetList;
 
     #endregion
 
@@ -15,6 +19,7 @@
 
     public int SelectedIfcIndex { get { return this.selectedIfcIndex; } set { this.selectedIfcIndex = value; } }
     public string SelectedIfcId { get { return this.selectedIfcId; } set { this.selectedIfcId = value; } }
+    public Dictionary<string, string> TargetList { get { return this.targetList; } set { this.targetList = value; } }
 
     #endregion
 
