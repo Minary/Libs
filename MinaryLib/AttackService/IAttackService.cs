8cd3f299c2feff34adffe81c642666d6519e0e96
Ruben Unteregger <ruben.unteregger@gmail.com>
2017-11-26 01:01:34 +0100

    Add AttackServiceHost class.

--- MinaryLib/AttackService/IAttackService.cs
+++ MinaryLib/AttackService/IAttackService.cs
@@ -5,17 +5,28 @@ namespace MinaryLib.AttackService
   public interface IAttackService
   {
 
+    #region PROPERTIES
+
     string ServiceName { get; set; }
 
     string WorkingDirectory { get; set; }
+
+    IAttackServiceHost AttackServiceHost { get; set; }
 
     Dictionary<string, SubModule> SubModules { get; set; }
 
     ServiceStatus Status { get; set; }
 
+    #endregion
+
+
+    #region PUBLIC
+
     ServiceStatus StartService(ServiceParameters serviceParameters);
 
     ServiceStatus StopService();
+
+    #endregion
 
   }
 }
